0ded714e7793155f7c3c60874bc6c739d782ebce
mrinalkaushik <111625574+mrinalkaushik@users.noreply.github.com>
2022-09-20 11:12:01 +0530

    Add files via upload

---
+++ Cmpstring.java
@@ -0,0 +1,13 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class Cmpstring {
+    public static void main(String[]arg){
+        String statement1=" my name is mrinal ";
+        String statement2="my name is mrinal ";
+
+       if (statement1==statement2);
+       { System.out.println("are common string");
+        }
+    }
+    
+}
